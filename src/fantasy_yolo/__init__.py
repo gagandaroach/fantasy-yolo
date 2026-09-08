697d119e59e0c4fbc95cce2e8fb1ffdde7aa2cc5
gagan <gagandaroach@gmail.com>
2026-09-08 06:27:48 +0000

    Add project scaffold and CI

---
+++ src/fantasy_yolo/__init__.py
@@ -0,0 +1,3 @@
+"""A programmatic interface to your own ESPN Fantasy Football team."""
+
+__version__ = "0.1.0"
---
+++ tests/test_smoke.py
@@ -0,0 +1,4 @@
+def test_package_imports():
+    import fantasy_yolo
+
+    assert fantasy_yolo.__version__
